64c241ea8e5b5280fa870f2a5abdfb7769af5162
Avinesh Singh <avin3sh@gmail.com>
2019-01-14 21:02:03 +0530

    Updates

---
+++ precache-manifest.61b2de9243433d7c87cdefc6f85bdeca.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "04f24fcbe456ae7a7b59",
+    "url": "/simple-report-listing/static/js/runtime~main.04f24fcb.js"
+  },
+  {
+    "revision": "d6d9725f99b8308e092b",
+    "url": "/simple-report-listing/static/js/main.d6d9725f.chunk.js"
+  },
+  {
+    "revision": "b3dfc61b87f2bcbb5f2b",
+    "url": "/simple-report-listing/static/js/1.b3dfc61b.chunk.js"
+  },
+  {
+    "revision": "f59ab61b64b8211df229626ee4b1a04d",
+    "url": "/simple-report-listing/index.html"
+  }
+];
